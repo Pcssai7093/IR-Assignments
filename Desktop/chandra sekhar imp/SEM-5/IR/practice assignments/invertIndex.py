0a0597d985d8535a5721f9eda5faaec07a8c20ef
raja <pcssai7093@gmail.com>
2022-08-22 19:16:51 +0530

    2nd commit

--- Desktop/chandra sekhar imp/SEM-5/IR/practice assignments/invertIndex.py
+++ Desktop/chandra sekhar imp/SEM-5/IR/practice assignments/invertIndex.py
@@ -1,24 +1,48 @@
 import os
-class list:
-    def __init__(self,docId):
-        self.docId=docId
-        self.list=None
+import re
 
-class listhead:
-    def __init__(self,word):
-        self.word=word
-        self.list=None
+# * using dictionary ds for posting list
+postingList=dict()
 
+
+# * importing stop words
+stopWords=[]
+file1=open("stopWords.csv",'r',encoding="utf8")
+stopWords=file1.read().split(',')
+
+
+# * setting path for reading files
 path="C:/Users/chandra sekhar/Desktop/chandra sekhar imp/SEM-5/IR/practice assignments/th-dataset"
 os.chdir(path)
-invertedIndex=[]
 
+
+# * function to read file and preprocession and populating as posting list
 def read_file(file_path):
     with open(file_path,'r',encoding="utf8") as f:
-        print(f.read())
-        print("\n")
+        words=f.read().split(" ")
+        # * removing stopwords
+        for str in words:
+            if str in stopWords:
+                words.remove(str)
+        
+        # * striping and removing redundaant characters
+        str=re.sub("[^a-zA-Z0-9 \n\.]","",str)
+        str=str.replace('.',"")
+        str=str.strip()
+        str=str.replace('.',"")
 
+        # * populating into posting list
+        if(postingList.get(str)==None):
+            postingList[str]=[f.name.split(path+'/')[1]]
+        else:
+            postingList.get(str).append(f.name.split(path+'/')[1])
+       
 
+# * reading folder and invoking file read function
 for file in os.listdir():
-    file_path=f"{path}\{file}"
+    file_path=f"{path}/{file}"
     read_file(file_path)
+
+# * printing posting list
+for key,value in postingList.items():
+    print(f"{key} {value}")
